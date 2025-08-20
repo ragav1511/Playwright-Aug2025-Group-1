0fb004e5ec1ca842e68f473c20dbd3e7c61bc321
ragav1511 <ragav.gokul@gmail.com>
2025-08-20 23:02:18 +0530

    Create primitivedatatypes.js

---
+++ primitivedatatypes.js
@@ -0,0 +1,27 @@
+var city="I live in Chennai"
+console.log(city)
+console.log(typeof city)
+console.log("----------------")
+const marks=85
+console.log(marks+10)
+console.log(typeof marks)
+console.log("----------------")
+let isWeekend=true
+if(isWeekend){
+    console.log("Relax")
+    console.log(isWeekend)
+    console.log("----------------")
+}else
+    console.log("Work")
+let futuregoal
+console.log(futuregoal)
+console.log(typeof futuregoal)
+console.log("----------------")
+
+/*
+var can be redeclared and reassigned. It is global scoping.
+let cannot be redeclared but can be reassigned. It is block scoping.
+const cannot be redeclared and reassigned. It is block scoping 
+*/
+
+
